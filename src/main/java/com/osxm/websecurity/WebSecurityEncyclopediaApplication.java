f66c7ea86280cd0736187ead485d3dc38268ea8f
osxm <oscar999@mail.ustc.edu.cn>
2022-09-02 07:18:12 +0800

    Init

---
+++ src/main/java/com/osxm/websecurity/WebSecurityEncyclopediaApplication.java
@@ -0,0 +1,13 @@
+package com.osxm.websecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecurityEncyclopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecurityEncyclopediaApplication.class, args);
+	}
+
+}

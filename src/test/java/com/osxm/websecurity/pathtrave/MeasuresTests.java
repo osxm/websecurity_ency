bd94f271dceb12471d2ae018dbfd0e3c8e28e0b4
Chen XM <oscar999@mail.ustc.edu.cn>
2022-12-23 22:44:47 +0800

    ok

---
+++ src/test/java/com/osxm/websecurity/pathtrave/MeasuresTests.java
@@ -0,0 +1,31 @@
+/**  
+* @Title: MeasuresTests.java
+* @Package com.osxm.websecurity.pathtrave
+* @Description: TODO
+* @author XM
+* @date 2022年10月13日 上午6:47:07
+* @Copyright: 2022
+* @version V1.0  
+*/
+package com.osxm.websecurity.pathtrave;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+public class MeasuresTests {
+
+	
+	@Test
+	public void normalize() {
+		Path path = Paths.get("/usr/local/../../etc/passwd").normalize(); 
+		System.out.println(path.toAbsolutePath());
+	}
+	
+	
+	@Test
+	public void getCanonicalPath() {
+		File file = new File();
+	}
+}

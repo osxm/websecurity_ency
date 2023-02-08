011b5c81bbd02607e9a856076a9c74bb729dc5e4
Chen XM <oscar999@mail.ustc.edu.cn>
2023-02-08 21:17:23 +0800

    Dead local Store

---
+++ src/main/java/com/osxm/weekness/low/DeadLocalStore.java
@@ -0,0 +1,21 @@
+/**  
+* @Title: DeadLocalStore.java
+* @Package com.osxm.weekness.low
+* @Description: TODO
+* @author XM
+* @date 2023年2月8日 下午9:02:02
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.weekness.low;
+
+import com.osxm.websecurity.model.User;
+
+public class DeadLocalStore {
+	
+	public void dls(User user) {
+		String s ="123";
+		String username = user.getName();
+	}
+
+}

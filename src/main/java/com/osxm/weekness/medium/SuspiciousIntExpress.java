1fbb656dd49b6c329c57364df0fc83e698eab035
OSXM <oscar999@mail.ustc.edu.cn>
2023-07-23 11:56:37 +0800

    sus int express

---
+++ src/main/java/com/osxm/weekness/medium/SuspiciousIntExpress.java
@@ -0,0 +1,29 @@
+/**  
+* @Title: SuspiciousIntExpress.java
+* @Package com.osxm.weekness.medium
+* @Description: TODO
+* @author XM
+* @date 2023年7月23日 上午11:41:18
+* @Copyright: 2023
+* @version V1.0  
+*/
+package com.osxm.weekness.medium;
+
+import java.util.List;
+
+/**
+ * @ClassName SuspiciousIntExpress
+ * @Description TODO
+ * @author XM 
+ * @date 2023年7月23日
+ * 
+ */
+public class SuspiciousIntExpress {
+
+	public void suspiciousIngegerExpression(List<String> list){
+		if(list == null ||list.size()<0) {
+			System.out.println("Hello.");
+		}
+		
+	}
+}
